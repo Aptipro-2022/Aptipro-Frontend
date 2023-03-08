3f3ca702ee740f71401a6b9b36a0ffe81aad5d18
haritha.t <haritha.t@edclub.in>
2023-03-08 16:38:00 +0530

    RTC-01-loginpage

--- src/app/services/questions.service.ts
+++ src/app/services/questions.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +16,11 @@ export class QuestionsService {
     return this.http.post('http://localhost:3000/addquestion', questiondetails, {responseType: 'text'}).subscribe(res => {
       console.log(res);
     })
+  }
+
+  getQuestions() : Observable<any>{
+    const url = environment.apiurl + 'question/getquestion';
+    return this.http.get(url);
   }
 
 }
